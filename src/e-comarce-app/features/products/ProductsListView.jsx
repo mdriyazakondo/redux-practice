1e822cfe9ab9550d4b3658d8fe42307cd98ee1dd
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-09-16 21:03:38 +0600

    added products

--- src/e-comarce-app/features/products/ProductsListView.jsx
+++ src/e-comarce-app/features/products/ProductsListView.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "./productsSlice";
 
 const ProductsListView = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { products, isLoading, error } = useSelector(
+    (state) => state.productsR
+  );
 
-export default ProductsListView
+  console.log(products);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
+
+  return (
+    <div className="max-w-7xl mx-auto  my-8">
+      {isLoading && <p>Loading.............</p>}
+      {error && <p>{error.message}</p>}
+      {!isLoading && !error && products.length > 0 && products && (
+        <section className="grid grid-cols-3 gap-4">
+          {products.map((product) => {
+            return (
+              <article
+                key={product.id}
+                className="border  p-4 rounded-md shadow"
+              >
+                <h3 className="text-2xl font-semibold text-gray-700">
+                  {product.title}
+                </h3>
+                <p className="text-gray-600 py-2">{product.description}</p>
+                <p className="text-xl font-medium ">Price: ${product.price}</p>
+                <button className="py-1.5 w-full bg-green-600 rounded-md text-white font-medium cursor-pointer mt-3 hover:bg-green-700 ">
+                  Add To Card
+                </button>
+              </article>
+            );
+          })}
+        </section>
+      )}
+    </div>
+  );
+};
+
+export default ProductsListView;
--- src/e-comarce-app/features/products/productsSlice.js
+++ src/e-comarce-app/features/products/productsSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   products: [],
@@ -6,10 +7,34 @@ const initialState = {
   error: null,
 };
 
+const BASE_URL = "http://localhost:3003/products";
+
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  async () => {
+    const res = await axios.get(BASE_URL);
+    return res.data;
+  }
+);
+
 const productsSlice = createSlice({
   name: "products",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchProducts.pending, (state) => {
+      state.isLoading = true;
+      state.error = null;
+    });
+    builder.addCase(fetchProducts.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.products = action.payload;
+    });
+    builder.addCase(fetchProducts.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = "Falid to fatch data" || action.error.message;
+    });
+  },
 });
 
 export default productsSlice.reducer;
